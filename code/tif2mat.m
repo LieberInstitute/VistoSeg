11eef8373b1b09b2cf1232d15de200ce8863746e
madhavitippani <madhavitippani28@gmail.com>
2023-07-05 13:15:48 -0400

    add comments to code

--- code/tif2mat.m
+++ code/tif2mat.m
@@ -1,4 +1,5 @@
-function tif2mat(fname)
+function tif2mat(fname) %function to convert segmented tif file to mat format
+%fname is the path and name of the segmented tif file
 imgInfo = imfinfo(fname);
 N = size(imgInfo,1);
 
@@ -7,4 +8,5 @@ save([fname(1:end-4), '.mat'],'channel1','-v7.3')
 
 if N>2
 for i=2:N, save([fname(1:end-4), '.mat'],sprintf('channel%d',i),"-append"); end
+%output mat file is saved at the same location as the input tif file
 end
