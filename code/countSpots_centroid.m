ec55bfde52ca89a0a78cfd4defa648be20aa81d2
madhavitippani <madhavitippani28@gmail.com>
2023-07-31 16:22:02 -0400

    make comments in code for version incompatibility

--- code/countSpots_centroid.m
+++ code/countSpots_centroid.m
@@ -33,7 +33,10 @@ for i = 1:nSpots
     spot = regionprops(mask==idx);
       for C = 1:numel(O)
         signal = struct2table(regionprops(mask==idx & BW.(O{C})>0));
+        %comment line 37 and uncomment lines 38 39 if MATLAB version<2021
         points = struct2table(regionprops(BW.(O{C}), 'Centroid')).Centroid;
+%         points = struct2table(regionprops(BW.(O{C}), 'Centroid'));
+%         points = points.Centroid;
         if isempty(points)
             isincircle = 0;
         else 
